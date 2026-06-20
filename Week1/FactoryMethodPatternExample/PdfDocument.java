5e3b30d228d47b7e3438d52bd8a2fe5d5dce543d
sandeepyallaraddi17-cpu <sandeepyallaraddi17@gmail.com>
2026-06-20 22:16:53 +0530

    Add files via upload

---
+++ Week1/FactoryMethodPatternExample/PdfDocument.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sande
+ */
+public class PdfDocument implements Document{
+    public void create(){
+        System.out.print("\nPdf Document was created\n");
+    }
+}
