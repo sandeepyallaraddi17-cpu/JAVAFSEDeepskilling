ddcfe60b752ed5168af4ac68882fbb832fb60499
sandeepyallaraddi17-cpu <sandeepyallaraddi17@gmail.com>
2026-06-25 23:58:29 +0530

    Update Product.java

--- Week1/Data structures and Algorithms/E-commerce Search Function/Product.java
+++ Week1/Data structures and Algorithms/E-commerce Search Function/Product.java
@@ -1,14 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package dsa;
-
-/**
- *
- * @author sande
- */
-public class Product{
+ class Product{
 
     int productId;
     String productName;
