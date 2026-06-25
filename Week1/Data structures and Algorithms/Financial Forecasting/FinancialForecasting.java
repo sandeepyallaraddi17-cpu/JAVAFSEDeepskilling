bff68846445a4ce52899e25214eb3cd114ac9b0f
sandeepyallaraddi17-cpu <sandeepyallaraddi17@gmail.com>
2026-06-26 00:01:55 +0530

    Add files via upload

--- Week1/Data structures and Algorithms/Financial Forecasting/FinancialForecasting.java
+++ Week1/Data structures and Algorithms/Financial Forecasting/FinancialForecasting.java
@@ -1,1 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa;
 
+/**
+ *
+ * @author sande
+ */
+public class FinancialForecasting{
+
+    public static double futureValue(double value,double rate,int years){
+
+        if(years==0){
+            return value;
+        }
+
+        return futureValue(value*(1+rate),rate,years-1);
+    }
+
+    public static void main(String[] args){
+
+        double currentValue=10000;
+        double growthRate=0.10;
+        int years=5;
+
+        double result=futureValue(currentValue,growthRate,years);
+
+        System.out.println("Current Value : "+currentValue);
+        System.out.println("Growth Rate : "+(growthRate*100)+"%");
+        System.out.println("Years : "+years);
+        System.out.println("Future Value : "+result);
+    }
+}
