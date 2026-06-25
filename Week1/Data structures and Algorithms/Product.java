63a0eb2072911b0b933e991da1e480743c0c0801
sandeepyallaraddi17-cpu <sandeepyallaraddi17@gmail.com>
2026-06-25 22:56:27 +0530

    Update Product.java

--- Week1/Data structures and Algorithms/Product.java
+++ Week1/Data structures and Algorithms/Product.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package dsa;
-
-/**
- *
- * @author sande
- */
 public class Product{
 
     int productId;
