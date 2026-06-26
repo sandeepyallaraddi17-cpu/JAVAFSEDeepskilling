7fdccd444b7cfe0db17254ba4ffb7683aa641f65
sandeepyallaraddi17-cpu <sandeepyallaraddi17@gmail.com>
2026-06-26 22:58:18 +0530

    Add files via upload

---
+++ Week1/PL SQL Programming/Control Structures/Scenario2.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Customers (
+    CustomerID NUMBER PRIMARY KEY,
+    Name VARCHAR2(100),
+    DOB DATE,
+    Balance NUMBER,
+    LastModified DATE
+);
+
+CREATE TABLE Loans (
+    LoanID NUMBER PRIMARY KEY,
+    CustomerID NUMBER,
+    LoanAmount NUMBER,
+    InterestRate NUMBER,
+    StartDate DATE,
+    EndDate DATE,
+    FOREIGN KEY (CustomerID)
+    REFERENCES Customers(CustomerID)
+);
